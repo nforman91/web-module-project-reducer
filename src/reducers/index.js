1072686ef5489db505e8ac0d616b1331c3d66c93
Nathan <nathan.forman@gmail.com>
2021-10-04 18:44:03 -0700

    Done

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,17 @@
-import { ADD_ONE, APPLY_NUMBER, CHANGE_OPERATION } from './../actions';
+import { 
+    ADD_ONE, 
+    APPLY_NUMBER, 
+    CHANGE_OPERATION, 
+    CLEAR_DISPLAY, 
+    MEMORY_ADD, 
+    MEMORY_RECALL, 
+    MEMORY_CLEAR
+} from './../actions';
 
 export const initialState = {
-    total: 100,
-    operation: "*",
-    memory: 100
+    total: 0,
+    operation: "+",
+    memory: 0
 }
 
 const calculateResult = (num1, num2, operation) => {
@@ -20,6 +28,7 @@ const calculateResult = (num1, num2, operation) => {
 const reducer = (state, action) => {
     switch(action.type) {
         case(ADD_ONE):
+            // console.log('ADD ONE REDUCER CALLED')
             return({
                 ...state,
                 total: state.total + 1
@@ -35,6 +44,26 @@ const reducer = (state, action) => {
             return ({
                 ...state,
                 operation: action.payload
+            });
+        case (CLEAR_DISPLAY):
+            return ({
+                ...state,
+                total: 0
+            });
+        case (MEMORY_ADD):
+            return ({
+                ...state,
+                memory: state.total
+            });
+        case (MEMORY_RECALL):
+            return ({
+                ...state,
+                total: calculateResult(state.memory, state.total, state.operation)
+            });
+        case (MEMORY_CLEAR):
+            return ({
+                ...state,
+                memory: 0
             });
             
         default:
